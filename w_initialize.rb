dac2102485b9cf04f058196246e514acfbc85e86
cep104 <candicelizabethpeters@gmail.com>
2021-03-11 10:33:07 -0600

    notes

--- w_initialize.rb
+++ w_initialize.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
-class Recipe
+class Pokemon
 
     @@all = []
 
     #gives us a reader and writer
-    attr_accessor :ingredients, :cook_time, :title
+    attr_accessor :moves, :type, :name
 
 
     # key: value
@@ -13,8 +13,10 @@ class Recipe
 
 
     #upon object creation, these are rules i want you to follow 
-    #.new will invoke initialize/ class method 
-    #intitialize / instance method / called on the instance itself 
+    #.new will invoke initialize
+#    what kind of method is .new?
+#what kind of method is an initialize? (instance no self before)
+    #intitialize / instance   method / called on the instance itself 
     def initialize(args_hash)
         #mass assignment/ metaprogramming/ most optimal 
         args_hash.each do |key, value|
@@ -44,5 +46,56 @@ end
 
 
 
-r = Recipe.new({cook_time: "2 hours", ingredients: ["oil", "herb"], title: "turkey", price: "2.00"})
+r = Recipe.new({name: "Charlizard", ingredients: ["oil", "herb"], title: "turkey", price: "2.00"})
+
+def initialize(moves, type, name)
+    @moves = moves
+    @type = type
+    @name = name
+end
+-would this work? without attr_accessor? 
+try it out in pry 
+-create a Pokemon.new see what errors we get 
+-when you call .new it is expecting for you to follow the rules you are setting up 
+-create with values 
+-show how the order matters when entering arguments. 
+-show how it cleans up our code that one line is same as other 4 lines. 
+-show that declairing instance variables does not depend on an attr_accessor but how you can't change or access with out them. 
+- there is a more efficent way than doing our instance variables. They may not show it in the lesson but I want to show you. 
+self.name = name
+self.type = type
+self.moves = moves
+what am I invoking here by calling .name = ? 
+this is a more efficant way to do this by invoking the attr_accessor. 
+    show how to do default valuables (setting to nil to avoid argument error and still be able to set those properties)
+
+show how to make it even more dynamic? 
+change to keys 
+def initialize(name:, moves:, type: )
+-set as keys 
+end
+
+p = Pokemon.new(name: "charmeleon", moves: ["Ember", "Fire Spin", "Scratch"], type: "Fire")
+-set to keys and now you can change the order in initilize. 
+-make sure you use the right key value pairs 
+
+best way to initialize and the most flexable is passing in hashes and mass assignment. 
+
+def initialize(args_hash)
+    how to I access a hashes key value?
+@name = args_hash[:name]
+end
+#wrap in hash
+p = Pokemon.new({name: "charmeleon", moves: ["Ember", "Fire Spin", "Scratch"], type: "Fire"})
+using mass assignment what if you have 30 different attrubues??
+ -still pass a hash 
+ -iterate over the hash and use method called send which will use a setter method to set those keys to those values
+def initialize(args_hash)
+args_hash.each do |key, value|
+ self.send("#{key}=", value)
+ -interpolate over the key, we do this because we want to add that = to it. ex. name= 
+    - what this does is sets that key to the value we are passing in the hash. So now we can initialize with a ton of properties (instance variables). 
+end
+end
+
 
--- wo_initialize.rb
+++ wo_initialize.rb
@@ -1,24 +1,54 @@
 require 'pry'
 
-class Recipe
+class Pokemon
+#even in its basic form we can create objects
+#if i wanted to add properties to it right now could I? 
+#if not why? 
+#Biggest misconception people have is that you have to include an initialize method you don't have to
+    
+#gives us a reader and writer
+    # attr_accessor :moves, :type, :name
 
-    #gives us a reader and writer
-    attr_accessor :title, :ingredients, :cook_time
+    def name=(name) #.title=
+        @name = name
+    end 
 
-    # def title=(title) #.title=
-    #     @title = title
-    # end 
-
-    # def title 
-    #     @title
-    # end 
+    def name
+        @name
+    end 
 
 
 binding.pry
 end 
+#show how it can get messy
+p1 = Pokemon.new
+p1.name = "Charmander"
+p1.moves = ["Ember", "Fire Spin", "Scratch"]
+p1.type = "Fire"
+p1 
 
-r = Recipe.new
-r.title = "Turkey"
-r.ingredients = ["oil", "butter", "herbs"]
-r.cook_time = "1"
-r 
+1. go over read me and what we went over Tuesday
+
+2. create a class
+
+3. ask if you could create an instance with just how it is set up now? 
+
+4. if i wanted to add properties to it right now could  I? if not why?
+
+5. talk about how you don't always need an initialize method. 
+
+6.walk through doing a reader and a writer (old way) show how it gets messy 
+
+p1 = Pokemon.new
+p1.name = "Charmander"
+p1.moves = ["Ember", "Fire Spin", "Scratch"]
+p1.type = "Fire"
+p1 
+
+7. initialize object then use dot notation to add properties this is important because in your assesment you may get asked to break apart your initialize method and create your properties without the initialize method. 
+
+8. talk about macros 
+attr_accessor, attr_writter, attr_reader 
+use symbols to put what we want our setter and getters to be. (look at document for macros)
+
+look at document about initialization 
